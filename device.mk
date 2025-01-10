0c9f9ed3e8f4d14f97394d722659640becad2055
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2025-01-09 19:40:47 -0500

    gta9p: device: build IPA

--- device.mk
+++ device.mk
@@ -32,6 +32,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
 
+# IPA
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libxml2 \
