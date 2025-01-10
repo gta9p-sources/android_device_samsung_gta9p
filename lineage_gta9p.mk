7b0b55de3c3d916e9ec1c8b0960814995f847764
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2025-01-09 19:46:30 -0500

    gta9p: lineage_gta9p: call inherit tablet config

--- lineage_gta9p.mk
+++ lineage_gta9p.mk
@@ -19,9 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 
-# Inherit from qssi device
+# Inherit from gta9p device
 $(call inherit-product, device/samsung/gta9p/device.mk)
 
 PRODUCT_DEVICE := gta9pwifi
