aed1cf5cbaec3cd44418e0624c6126b84ee2742f
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2025-01-09 19:37:55 -0500

    gta9p: device: copy telephony permissions

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ PRODUCT_PACKAGES += \
     init.vendor.rilchip.rc \
     init.vendor.rilcommon.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libxml2 \
